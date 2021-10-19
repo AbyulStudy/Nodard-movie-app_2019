be081467c7d94a70afb1f227b65fcc653e403d81
Abyul <kinsng1599@gmail.com>
2021-10-19 20:18:23 +0900

    [UPDATE] App.js to PropTypes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,74 @@
+// npm i porp-types
+import PropTypes from "prop-types";
+
+function Food({name , picture, rating}){
+  return (
+    <div>
+      <h2>{name}</h2>
+      <h3>{rating}/5.0</h3>
+      <img src={picture} alt={name} />
+    </div>
+  )
+}
+
+//props type
+// isRequired 필수정보
+// https://ko.reactjs.org/docs/typechecking-with-proptypes.html
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired 
+}
+
+const foods = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating: 5.0
+    },
+  {
+    id: 2,
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+    rating: 4.4
+    },
+  {
+    id: 3,
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+    rating: 3.8
+    },
+  {
+    id: 4,
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: 4.0
+    },
+  {
+    id: 5,
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+    rating: 4.0
+    }
+];
+
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      {foods.map((food) => (
+        <Food
+          key={food.id}
+          name={food.name}
+          picture={food.image}
+          rating={food.rating}
+        />
+      ))}
     </div>
   );
 }
